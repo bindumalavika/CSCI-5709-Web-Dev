2f578a7928841f34ffa493c10e7431555763ef51
koyalkar <bindumalavika24@gmail.com>
2025-05-30 10:00:39 -0300

    tutorial-2: Angular routing, state management and lifecycle hooks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tutotial-1' title`, () => {
+  it(`should have the 'tutorial-2' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tutotial-1');
+    expect(app.title).toEqual('tutorial-2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tutotial-1');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, tutorial-2'
+    );
   });
 });
